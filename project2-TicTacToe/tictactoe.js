d43b1b718101a1fb50e61ad644ef1d7c1d566e79
zeFloor <erviss1@gmail.com>
2021-12-13 18:13:59 +0200

    added some more functionality

--- project2-TicTacToe/tictactoe.js
+++ project2-TicTacToe/tictactoe.js
@@ -1,4 +1,5 @@
 let players = []
+let turn = 0
 
 const player = (name, sign) => {
     let playerName = name
@@ -11,12 +12,23 @@ const gameBoard = (() => {
     let playerX = []
     let playerO = []
 
-    const addXnO = () => {}
+    const addXnO = (sign, slot) => {
+        if(sign == 'X')
+        playerX.push(slot)
+        else
+        playerO.push(slot)
+        
+        _printXnO(sign, slot)
+    }
     const checkEmptyCell = () => {}
-    const printXnO = () => {}
+    const _printXnO = (sign, slot) => {
+        document.querySelector(`.${slot}`).innerHTML = `<p>${sign}</p>`
+        }
     const checkIfWin = () => {}
-})()
+    const boardReset = () => {}
 
+    return {addXnO, checkIfWin, boardReset}
+})()
 
 const displayController = (() => {
     const checkPlayers = (e) => {
@@ -24,9 +36,30 @@ const displayController = (() => {
         alert('Please type in your names')
         else {
            players.push(player(document.querySelector('.player1').value, 'X'))
-           players.push(player(document.querySelector('.player2').value, 'Y'))
+           players.push(player(document.querySelector('.player2').value, 'O'))
            console.log(players)
         }
     }
-    return {checkPlayers}
+
+    const _placedSymbol = (() => {
+        const cells = document.querySelectorAll('#cell')
+        cells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                if(cell.innerText !== '') {
+                    alert("This slot is taken!")
+                } else {
+                    if(turn % 2 == 0) {
+                        gameBoard.addXnO(players[0].playerSign, cell.className)
+                    } else {
+                        gameBoard.addXnO(players[1].playerSign, cell.className)
+                    }
+                    turn++
+                }
+                
+            })
+        });
+    })()
+
+    return {checkPlayers,}
 })()
+
