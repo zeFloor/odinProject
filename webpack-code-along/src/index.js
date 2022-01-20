e8541a22f27e98d987c178fadcd83adc9b65a950
zeFloor <erviss1@gmail.com>
2022-01-20 11:38:57 +0200

    cleaned up files for the output managment tutorial

--- webpack-code-along/src/index.js
+++ webpack-code-along/src/index.js
@@ -1,34 +1,9 @@
 import myName from './myName'
-import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml'
-import Notes from './data.csv'
-import toml from './data.toml'
-import yaml from './data.yaml'
-import json from './data.json5'
-
-console.log(toml.title)
-console.log(toml.owner.name)
-
-console.log(yaml.title)
-console.log(yaml.owner.name)
-
-console.log(json.title)
-console.log(json.owner.name)
 
 function component() {
     const element = document.createElement('div')
 
     element.innerHTML = myName('Ervis')
-    element.classList.add('hello')
-
-    const myIcon = new Image()
-    myIcon.src = Icon
-
-    element.appendChild(myIcon)
-
-    console.log(Data)
-    console.log(Notes)
 
     return element
 }
