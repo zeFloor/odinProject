090a03dd97ccf4e1c208f2cb41c154c8e6c9daae
zeFloor <erviss1@gmail.com>
2022-01-20 11:32:30 +0200

    imported data with webpack

--- webpack-code-along/src/index.js
+++ webpack-code-along/src/index.js
@@ -3,6 +3,18 @@ import './style.css'
 import Icon from './icon.png'
 import Data from './data.xml'
 import Notes from './data.csv'
+import toml from './data.toml'
+import yaml from './data.yaml'
+import json from './data.json5'
+
+console.log(toml.title)
+console.log(toml.owner.name)
+
+console.log(yaml.title)
+console.log(yaml.owner.name)
+
+console.log(json.title)
+console.log(json.owner.name)
 
 function component() {
     const element = document.createElement('div')
