c0ed12299a7e721a60f39ebd36f1748a650ed0b4
zeFloor <erviss1@gmail.com>
2022-01-20 10:35:09 +0200

    adding css with webpack

--- webpack-code-along/src/index.js
+++ webpack-code-along/src/index.js
@@ -1,9 +1,11 @@
 import myName from './myName'
+import './style.css'
 
 function component() {
     const element = document.createElement('div')
 
     element.innerHTML = myName('Ervis')
+    element.classList.add('hello')
 
     return element
 }
