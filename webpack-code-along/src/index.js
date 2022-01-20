c1cde078e0fd14b0d60beb614236aae74d0ef77c
zeFloor <erviss1@gmail.com>
2022-01-20 10:01:29 +0200

    used export and import

--- webpack-code-along/src/index.js
+++ webpack-code-along/src/index.js
@@ -1,9 +1,9 @@
-import _ from 'lodash'
+import myName from './myName'
 
 function component() {
     const element = document.createElement('div')
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.innerHTML = myName('Ervis')
 
     return element
 }
