cc66ad030f50d3b2d234267db9f9757ed7b2e8e6
zeFloor <erviss1@gmail.com>
2022-01-20 10:50:11 +0200

    processed image with webpack

--- webpack-code-along/src/index.js
+++ webpack-code-along/src/index.js
@@ -1,11 +1,17 @@
 import myName from './myName'
 import './style.css'
+import Icon from './icon.png'
 
 function component() {
     const element = document.createElement('div')
 
     element.innerHTML = myName('Ervis')
     element.classList.add('hello')
+
+    const myIcon = new Image()
+    myIcon.src = Icon
+
+    element.appendChild(myIcon)
 
     return element
 }
