2d24a5577738f3826c434ddbc04912295ac7019f
zeFloor <erviss1@gmail.com>
2022-01-28 09:56:11 +0200

    trying  output management

--- webpack-code-along/src/index.js
+++ webpack-code-along/src/index.js
@@ -1,9 +1,16 @@
 import myName from './myName'
+import printMe from './print.js'
 
 function component() {
     const element = document.createElement('div')
+    const btn = document.createElement('button')
 
     element.innerHTML = myName('Ervis')
+
+    btn.innerHTML = 'Click me and check the console'
+    btn.onclick = printMe
+
+    element.appendChild(btn)
 
     return element
 }
